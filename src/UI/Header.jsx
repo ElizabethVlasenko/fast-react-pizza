b69548002b8583ffcb744e8673959a7bd0e7654b
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-08-07 11:34:37 +0100

    Created a cart slice

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -4,15 +4,16 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between gap-2 border-b border-stone-200 bg-yellow-400 p-4 uppercase sm:p-6">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 p-4 uppercase sm:p-6 md:gap-64">
       <Link to="/" className="tracking-widest">
         Fast React Pizza&nbsp;Co.
       </Link>
 
-      <SearchOrder />
-      <div>
-        <Username />
+      <div className="md:absolute md:left-2/4 md:translate-x-[-50%]">
+        <SearchOrder />
       </div>
+
+      <Username />
     </header>
   );
 }
